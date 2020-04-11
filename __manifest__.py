8ba0d45c7baf1139bf49170410b6789758dd1965
jchanme <39323414+jchanme@users.noreply.github.com>
2020-04-11 14:13:30 +0800

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "SMTP Server Per User",
+    "version": "12.0.1.0.0",
+    "author": "Safyric Co., Ltd.",
+    "category": "Knowledge",
+    "license": "AGPL-3",
+    "website": "https://github.com/safyric/mail_smtp_per_user",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/ir_mail_server_views.xml",
+    ],
+    "installable": True,
+}
